5dc6bb7a98c8b16bce0c8784095f0b22295a4379
Lina1999 <evelineghazaryan@gmail.com>
2018-07-04 13:34:39 +0400

    Procedures Added

---
+++ UsersDB/Stored Procedures/UpdateCustomer.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[UpdateCustomer]
+    @Id int,
+	@Name varchar(30),
+	@Surname varchar(50),
+	@Email varchar(30),
+	@Login varchar(40),
+	@Password varchar(25),
+	@Roles_ID int
+AS
+	Update Customers
+	set [Name]=@Name, Surname=@Surname, Email=@Email, [Login]=@Login, [Password]=@Password, Roles_ID=@Roles_ID
+	where Id=@Id
+GO
