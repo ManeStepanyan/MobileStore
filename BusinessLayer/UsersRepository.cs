7e1ee54f97f52bfd126003c5b303155f9e523793
manestep <manestep@gmail.com>
2018-07-08 14:11:34 +0400

    mapper changed

--- BusinessLayer/UsersRepository.cs
+++ BusinessLayer/UsersRepository.cs
@@ -8,13 +8,17 @@ namespace BusinessLayer
     public class UsersRepository
     {
         public UsersDAL DAL { get; }
+        IMapper mapper1;
+        IMapper mapper2;
+        IMapper mapper3;
+        IMapper mapper4;
 
         public UsersRepository()
         {
             this.DAL = new UsersDAL();
 
 
-            Mapper.Initialize(cfg => cfg.CreateMap<Seller, DALUsers.Seller>()
+             mapper1 = new MapperConfiguration(cfg => cfg.CreateMap<Seller, DALUsers.Seller>()
                 .ForMember("Id", opt => opt.MapFrom(c => c.Id))
                 .ForMember("Name", opt => opt.MapFrom(src => src.Name))
                 .ForMember("Password", opt => opt.MapFrom(src => src.Password))
@@ -22,9 +26,9 @@ namespace BusinessLayer
                 .ForMember("Login", opt => opt.MapFrom(src => src.Login))
                 .ForMember("CellPhone", opt => opt.MapFrom(src => src.CellPhone))
                 .ForMember("Address", opt => opt.MapFrom(src => src.Address))
-                .ForMember("Rating", opt => opt.MapFrom(src => src.Rating)));
+                .ForMember("Rating", opt => opt.MapFrom(src => src.Rating))).CreateMapper();
 
-            Mapper.Initialize(cfg => cfg.CreateMap<Customer, DALUsers.Customer>()
+             mapper2 = new MapperConfiguration(cfg => cfg.CreateMap<Customer, DALUsers.Customer>()
                 .ForMember("Id", opt => opt.MapFrom(c => c.Id))
                 .ForMember("Name", opt => opt.MapFrom(src => src.Name))
                 .ForMember("Password", opt => opt.MapFrom(src => src.Password))
@@ -32,19 +36,19 @@ namespace BusinessLayer
                 .ForMember("Login", opt => opt.MapFrom(src => src.Login))
                 .ForMember("Surname", opt => opt.MapFrom(src => src.Surname))
                 .ForMember("Email", opt => opt.MapFrom(src => src.Email))
-                .ForMember("Status", opt => opt.MapFrom(src => src.Status)));
+                .ForMember("Status", opt => opt.MapFrom(src => src.Status))).CreateMapper();
 
-            Mapper.Initialize(cfg => cfg.CreateMap<Admin, DALUsers.Admin>()
+             mapper3 = new MapperConfiguration(cfg => cfg.CreateMap<Admin, DALUsers.Admin>()
                 .ForMember("Id", opt => opt.MapFrom(c => c.Id))
                 .ForMember("Name", opt => opt.MapFrom(src => src.Name))
                 .ForMember("Password", opt => opt.MapFrom(src => src.Password))
                 .ForMember("Roles_ID", opt => opt.MapFrom(src => src.Roles_ID))
-                .ForMember("Login", opt => opt.MapFrom(src => src.Login)));
+                .ForMember("Login", opt => opt.MapFrom(src => src.Login))).CreateMapper();
 
-            Mapper.Initialize(cfg => cfg.CreateMap<Role, DALUsers.Role>()
+             mapper4 = new MapperConfiguration(cfg => cfg.CreateMap<Role, DALUsers.Role>()
                 .ForMember("Id", opt => opt.MapFrom(c => c.Id))
                 .ForMember("Name", opt => opt.MapFrom(src => src.Name))
-                .ForMember("Description", opt => opt.MapFrom(src => src.Description))); 
+                .ForMember("Description", opt => opt.MapFrom(src => src.Description))).CreateMapper(); 
         }
 
         public bool SellerSignUp(Seller seller)
@@ -96,21 +100,21 @@ namespace BusinessLayer
         {
             //var map = new ReflectionBasedMapper<Seller, DALUsers.Seller>();
             //return map.MapBack(this.DAL.GetSellerByID(id));
-            return Mapper.Map<DALUsers.Seller, Seller>(this.DAL.GetSellerByID(id));
+            return mapper1.Map<DALUsers.Seller, Seller>(this.DAL.GetSellerByID(id));
         }
 
         public Seller GetSeller(string login)
         {
             //var map = new ReflectionBasedMapper<Seller, DALUsers.Seller>();
             //return map.MapBack(this.DAL.GetSellerByName(login));
-            return Mapper.Map<DALUsers.Seller, Seller>(this.DAL.GetSellerByName(login));
+            return mapper1.Map<DALUsers.Seller, Seller>(this.DAL.GetSellerByName(login));
         }
 
         public IEnumerable<Seller> GetAllSellers()
         {
             //var map = new ReflectionBasedMapper<Seller, DALUsers.Seller>();
             //return this.DAL.GetSellers().Select(a => map.MapBack(a));
-            return Mapper.Map<IEnumerable<DALUsers.Seller>, List<Seller>>(this.DAL.GetSellers());
+            return mapper1.Map<IEnumerable<DALUsers.Seller>, List<Seller>>(this.DAL.GetSellers());
         }
 
         public bool CustomerSignUp(Customer customer)
@@ -163,21 +167,21 @@ namespace BusinessLayer
         {
             //var map = new ReflectionBasedMapper<Customer, DALUsers.Customer>();
             //return map.MapBack(this.DAL.GetCustomerByID(id));
-            return Mapper.Map<DALUsers.Customer, Customer>(this.DAL.GetCustomerByID(id));
+            return mapper2.Map<DALUsers.Customer, Customer>(this.DAL.GetCustomerByID(id));
         }
 
         public Customer GetCustomer(string login)
         {
             //var map = new ReflectionBasedMapper<Customer, DALUsers.Customer>();
             //return map.MapBack(this.DAL.GetCustomerByName(login));
-            return Mapper.Map<DALUsers.Customer, Customer>(this.DAL.GetCustomerByName(login));
+            return mapper2.Map<DALUsers.Customer, Customer>(this.DAL.GetCustomerByName(login));
         }
 
         public IEnumerable<Customer> GetAllCustomer()
         {
             //var map = new ReflectionBasedMapper<Customer, DALUsers.Customer>();
             //return this.DAL.GetCustomers().Select(a => map.MapBack(a));
-            return Mapper.Map<IEnumerable<DALUsers.Customer>, List<Customer>>(this.DAL.GetCustomers());
+            return mapper2.Map<IEnumerable<DALUsers.Customer>, List<Customer>>(this.DAL.GetCustomers());
         }
 
         public bool AdminSignUp(Admin admin)
@@ -213,7 +217,7 @@ namespace BusinessLayer
         {
             //var map = new ReflectionBasedMapper<Admin, DALUsers.Admin>();
             //return map.MapBack(this.DAL.GetAdminByID(id));
-            return Mapper.Map<DALUsers.Admin, Admin>(this.DAL.GetAdminByID(id));
+            return mapper3.Map<DALUsers.Admin, Admin>(this.DAL.GetAdminByID(id));
         }
 
         //public Admin GetAdmin(string login)
@@ -226,7 +230,7 @@ namespace BusinessLayer
         {
             //var map = new ReflectionBasedMapper<Role, DALUsers.Role>();
             //return map.MapBack(this.DAL.GetRole(id));
-            return Mapper.Map<DALUsers.Role, Role>(this.DAL.GetRole(id));
+            return mapper4.Map<DALUsers.Role, Role>(this.DAL.GetRole(id));
         }
 
 
