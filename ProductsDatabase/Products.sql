a4556af3bec435aeacbe0509ab987b0f05ae7921
Lina1999 <evelineghazaryan@gmail.com>
2018-07-13 01:52:43 +0400

    ProductsDB added

---
+++ ProductsDatabase/Products.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(30) NOT NULL, 
+    [Brand] VARCHAR(20) NOT NULL, 
+    [Version] DECIMAL(5, 3) NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [RAM] INT NOT NULL, 
+    [Year] INT NOT NULL, 
+    [Display] INT NOT NULL, 
+    [Battery] VARCHAR(30) NOT NULL, 
+    [Camera] INT NOT NULL, 
+    [Image] VARCHAR(200) NOT NULL, 
+    [Seller's_ID] INT NOT NULL
+)
