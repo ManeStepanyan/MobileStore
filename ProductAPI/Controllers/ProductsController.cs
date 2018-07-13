d7b4e96c6bd5ecf754b9e004e761cbbc73ffc55f
Lina1999 <evelineghazaryan@gmail.com>
2018-07-13 20:46:30 +0400

    ProductAPI added

---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly Repo<Product> repository;
+        private readonly Repo<ProductPublicInfo> publicRepository;
+
+        public ProductsController(Repo<Product> repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
