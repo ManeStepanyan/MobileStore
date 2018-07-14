6fcc800b0b19467fe3f3e3dbd8e0bd47202994f9
Lina1999 <evelineghazaryan@gmail.com>
2018-07-14 18:03:28 +0400

    OrdersDB added

---
+++ OrdersDB/Orders.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Date] DATE NOT NULL,
+	[Address] VARCHAR(50) NOT NULL, 
+    [CellPhone] VARBINARY(50) NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Product_ID] INT NOT NULL, 
+    [Customer_ID] INT NOT NULL, 
+    [TotalAmount] MONEY NOT NULL
+)
