4ce4014b5037aef56809a69aaf8f23f7a65b4918
Lina1999 <evelineghazaryan@gmail.com>
2018-07-14 16:43:45 +0400

    SelectProduct added

--- ProductsDatabase/StoredProcedures/SelectProducts.sql
+++ ProductsDatabase/StoredProcedures/SelectProducts.sql
@@ -1,27 +1,23 @@
 ﻿CREATE PROCEDURE [dbo].[SelectProducts]
-    @Name VARCHAR(30), 
-    @Brand VARCHAR(20), 
-    @Version DECIMAL(5, 3), 
-    @Price MONEY, 
-    @RAM INT, 
-    @Year INT, 
-    @Display INT, 
-    @Battery VARCHAR(30), 
-    @Camera INT, 
-    @Image VARCHAR(200)
+    @Name VARCHAR(30)=null, 
+    @Brand VARCHAR(20)=null, 
+    @Version DECIMAL(5, 3)=null, 
+    @Price MONEY=null,
+    @RAM INT =null,
+    @Year INT =null,
+    @Display INT =null,
+    @Battery VARCHAR(30) =null,
+    @Camera INT =null,
+    @Image VARCHAR(200)=null
 AS
-	declare @sqlName nvarchar(100)
-	declare @sqlBrand nvarchar(100)
-
-	if(@Name is not null)
-		select @sqlName='select *  from Products where [Name]=@Name'
-	else
-		select @sqlName='select *  from Products'
-	if(@Brand is not null)
-		select @sqlBrand='select *  from Products where [Brand]=@Brand'
-	else
-		select @sqlBrand='select *  from Products'
-	select @sqlName
+	declare @Name1 varchar(30)
+	declare @Brand1 varchar(20)
+	select @Name1= iif(@Name is null, [Name], @Name)
+	select @Brand1= iif(@Brand is null, [Brand], @Brand)
+	from Products
+	select * from Products
+    intersect
+	select  *  from Products where [Name]=@Name1 	
 	intersect
-	select @sqlBrand
+	select *  from Products where [Brand]=@Brand1 
 GO
