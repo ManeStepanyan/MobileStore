53a08a932d940316adbb7e10c9057c9dd35dda25
Lina1999 <evelineghazaryan@gmail.com>
2018-07-16 20:05:41 +0400

    ProductId deleted

--- OrdersDB/Orders.sql
+++ OrdersDB/Orders.sql
@@ -5,7 +5,5 @@
 	[Address] VARCHAR(50) NOT NULL, 
     [CellPhone] VARBINARY(50) NOT NULL, 
     [Quantity] INT NOT NULL, 
-    [Product_ID] INT NOT NULL, 
-    [Customer_ID] INT NOT NULL, 
     [TotalAmount] MONEY NOT NULL
 )
--- OrdersDB/StoredProcedures/GetOrderByProductId.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[GetOrderByProductId]
-	@Id int
-AS
-	select * from Orders where Product_ID=@Id	
-GO
