6562728814edb4347c975d7664c4ce5167d3352e
Lina1999 <evelineghazaryan@gmail.com>
2018-07-18 21:10:20 +0400

    Version type changed

--- ProductAPI/Models/Product.cs
+++ ProductAPI/Models/Product.cs
@@ -5,7 +5,7 @@
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
-        public double? Version { get; set; }
+        public string Version { get; set; }
         public double? Price { get; set; }
         public int? RAM { get; set; }
         public int? Year { get; set; }
